4e773c9b60c35bb53a0c9ccbb4c27480a205ca02
whn <me@whn.se>
2020-09-17 18:11:55 +0200

    auto deleting script

--- setup.sh
+++ setup.sh
@@ -8,5 +8,6 @@ GH_PATH=$(git remote get-url origin | grep -E -i -w -o "[a-z]+/[a-z]+")
 
 go mod init github.com/$GH_PATH
 go get go.uber.org/fx
-git rm README.md
+git rm README.md setup.sh
 git add go.mod go.sum
+git commit -m "Bootstraped using setup.sh"
